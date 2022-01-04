c29fcc1bfc1dda7ab95cc2ec9a2fe9428e5b8bc0
Stackbit Code Editor <projects@stackbit.com>
2022-01-04 23:42:28 +0000

    index.tsx: changed

--- src/components/molecules/ImageBlock/index.tsx
+++ src/components/molecules/ImageBlock/index.tsx
@@ -22,6 +22,7 @@ export default function ImageBlock(props) {
             className={classNames('sb-component', 'sb-component-block', 'sb-component-image-block', cssClasses)}
             src={url}
             alt={altText || ''}
+            
             data-sb-field-path={annotations.join(' ').trim()}
         />
     );
