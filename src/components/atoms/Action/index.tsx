369274cfd8a39d2fc6673fa71a57eef29f673029
Stackbit Code Editor <projects@stackbit.com>
2022-01-05 01:06:09 +0000

    index.tsx: changed

--- src/components/atoms/Action/index.tsx
+++ src/components/atoms/Action/index.tsx
@@ -81,7 +81,7 @@ export default function Action(props) {
             {showIcon && IconComponent && (
                 <IconComponent
                     className={classNames('fill-current h-5 w-5', {
-                        'order-first': iconPosition === 'right',
+                        'order-first': iconPosition === 'left',
                         'mr-1.5': label && iconPosition === 'left',
                         'ml-1.5': label && iconPosition === 'right'
                     })}
