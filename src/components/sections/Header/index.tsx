57bef8beece5da7dc134c763ac040ef96a141fcd
Stackbit Code Editor <projects@stackbit.com>
2022-01-05 18:12:50 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -176,7 +176,7 @@ function siteLogoLink(props) {
         <Link href="/" aria-label={props.title} className="sb-header-logo flex items-center">
             {props.logo && <ImageBlock {...props.logo} className={classNames('max-h-12', { 'mr-2': props.isTitleVisible })} data-sb-field-path=".logo" />}
             {props.title && props.isTitleVisible && (
-                <span className="text-2xl font-medium" data-sb-field-path=".title">
+                <span className="font-medium" data-sb-field-path=".title">
                     Indigenous Innovative Solutions Inc.
                 </span>
             )}
