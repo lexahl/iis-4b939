87b35fae0d8b7d077e720c25e5f16400ac463b43
Stackbit Code Editor <projects@stackbit.com>
2022-01-05 04:31:01 +0000

    index.tsx: changed

--- src/components/layouts/PostLayout/index.tsx
+++ src/components/layouts/PostLayout/index.tsx
@@ -76,7 +76,7 @@ function PostAttribution({ post }) {
         <span>
             {author && (
                 <>
-                    {', by '}
+                    {''}
                     {author}
                 </>
             )}
