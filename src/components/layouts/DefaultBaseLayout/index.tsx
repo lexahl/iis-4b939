bebf9a24c78e0f24e825f399d15c0b46a0c8542a
Stackbit Code Editor <projects@stackbit.com>
2022-01-05 16:50:23 +0000

    index.tsx: changed

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -16,6 +16,14 @@ export default function DefaultBaseLayout(props) {
                     <title>{page.title}</title>
                     <meta name="description" content="Stackbit Theme" />
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <meta name="title" content="Indigenous Innovative Solutions Inc.">
+<meta name="description" content="Indigenous and social innovation, entrepreneurial mentorship, and community determination to empower Indigenous people to design their own future.">
+<meta name="keywords" content="Indigenous, Canada, Community">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="English">
+<meta name="author" content="Winona Lafreniere">
+
                     {site.favicon && <link rel="icon" href={site.favicon} />}
                 </Head>
                 {site.header && <Header {...site.header} annotationPrefix={siteMeta.id} />}
