a7f6ceba2a218417e5c75b634af78a3f9b2545b9
Stackbit Code Editor <projects@stackbit.com>
2022-01-05 18:54:26 +0000

    index.tsx: changed

--- src/components/molecules/ImageBlock/index.tsx
+++ src/components/molecules/ImageBlock/index.tsx
@@ -16,7 +16,7 @@ export default function ImageBlock(props) {
         `${annotationPrefix}.elementId#@id`
     ];
 
-    return (<center>
+    return (
         <img
             id={cssId}
             className={classNames('sb-component', 'sb-component-block', 'sb-component-image-block', cssClasses)}
