90e02a18e32bf540fcb3e36136a97b1255e1b810
Stackbit Code Editor <projects@stackbit.com>
2022-01-06 01:03:05 +0000

    index.tsx: changed

--- src/components/sections/QuoteSection/index.tsx
+++ src/components/sections/QuoteSection/index.tsx
@@ -2,96 +2,250 @@ import * as React from 'react';
 import Markdown from 'markdown-to-jsx';
 import classNames from 'classnames';
 
-import { mapStylesToClassNames as mapStyles } from '../../../utils/map-styles-to-class-names';
-import { getDataAttrs } from '../../../utils/get-data-attrs';
-import { BackgroundImage } from '../../atoms';
+import { Social, Action, Link } from '../../atoms';
+import ImageBlock from '../../molecules/ImageBlock';
 
-export default function QuoteSection(props) {
-    const cssId = props.elementId || null;
+export default function Footer(props) {
     const colors = props.colors || 'colors-d';
-    const sectionStyles = props.styles?.self || {};
-    const sectionWidth = sectionStyles.width || 'wide';
-    const sectionHeight = sectionStyles.height || 'auto';
-    const sectionJustifyContent = sectionStyles.justifyContent || 'center';
+    const footerStyles = props.styles?.self || {};
+    const footerWidth = footerStyles.width || 'narrow';
     return (
-        <div
-            id={cssId}
-            {...getDataAttrs(props)}
-            className={classNames(
-                'sb-component',
-                'sb-component-section',
-                'sb-component-quote-section',
-                colors,
-                'flex',
-                'flex-col',
-                'justify-center',
-                'relative',
-                mapMinHeightStyles(sectionHeight),
-                sectionStyles.margin,
-                sectionStyles.padding || 'py-12 px-4',
-                sectionStyles.borderColor,
-                sectionStyles.borderStyle ? mapStyles({ borderStyle: sectionStyles.borderStyle }) : 'border-none',
-                sectionStyles.borderRadius ? mapStyles({ borderRadius: sectionStyles.borderRadius }) : null
-            )}
-            style={{
-                borderWidth: sectionStyles.borderWidth ? `${sectionStyles.borderWidth}px` : null
-            }}
+        <footer
+            className={classNames('sb-component', 'sb-component-footer', colors, footerStyles.padding || 'py-16 px-4')}
+            data-sb-field-path={`${props.annotationPrefix}:footer`}
         >
-            {props.backgroundImage && <BackgroundImage {...props.backgroundImage} />}
-            <div className={classNames('flex', 'relative', 'w-full', mapStyles({ justifyContent: sectionJustifyContent }))}>
-                <div className={classNames('w-full', mapMaxWidthStyles(sectionWidth))}>{quoteContent(props)}</div>
-            </div>
-        </div>
+            <div className={classNames('border-t', 'border-current', 'mx-auto', 'pt-16', mapMaxWidthStyles(footerWidth))}>{footerVariants(props)}</div>
+        </footer>
     );
 }
 
-function quoteContent(props) {
-    const styles = props.styles || {};
-    return (
-        <blockquote>
-            {props.quote && (
-                <Markdown
-                    options={{ forceBlock: true, forceWrapper: true }}
-                    className={classNames('sb-markdown', 'sm:leading-tight', 'text-4xl', 'sm:text-6xl', styles.quote ? mapStyles(styles.quote) : null)}
-                    data-sb-field-path=".quote"
-                >
-                    {props.quote}
-                </Markdown>
-            )}
-            {(props.name || props.title) && (
-                <footer className="mt-8 sm:mt-10">
-                    {props.name && (
-                        <span className={classNames('block', 'text-lg', styles.name ? mapStyles(styles.name) : null)} data-sb-field-path=".name">
-                            {props.name}
-                        </span>
-                    )}
-                    {props.title && (
-                        <span className={classNames('block', 'text-lg', styles.title ? mapStyles(styles.title) : null)} data-sb-field-path=".title">
-                            {props.title}
-                        </span>
-                    )}
-                </footer>
-            )}
-        </blockquote>
-    );
-}
-
-function mapMinHeightStyles(height) {
-    switch (height) {
-        case 'auto':
-            return 'min-h-0';
-        case 'screen':
-            return 'min-h-screen';
+function footerVariants(props) {
+    const variant = props.variant || 'variant-a';
+    switch (variant) {
+        case 'variant-a':
+            return footerVariantA(props);
+        case 'variant-b':
+            return footerVariantB(props);
     }
     return null;
+}
+
+function footerVariantA(props) {
+    const primaryLinks = props.primaryLinks || [];
+    const socialLinks = props.socialLinks || [];
+    const legalLinks = props.legalLinks || [];
+    return (
+        <>
+            {(props.logo || props.title || props.text) && (
+                <div className="mb-12">
+                    <Link href="/" className="sb-footer-logo flex items-center">
+                        {props.logo && <ImageBlock {...props.logo} className={classNames('max-h-12', { 'mr-2': props.title })} data-sb-field-path=".logo" />}
+                        {props.title && (
+                            <span className="text-3xl font-medium" data-sb-field-path=".title">
+                                {props.title}
+                            </span>
+                        )}
+                    </Link>
+                    {props.text && (
+                        <Markdown
+                            options={{ forceBlock: true, forceWrapper: true }}
+                            className={classNames('sb-markdown', 'max-w-xl', { 'mt-8': props.title || props.logo })}
+                            data-sb-field-path=".text"
+                        >
+                            {props.text}
+                        </Markdown>
+                    )}
+                </div>
+            )}
+            {(primaryLinks.length > 0 || socialLinks.length > 0 || props.contacts) && (
+                <div className="sm:flex sm:justify-between sm:items-end">
+                    {primaryLinks.length > 0 && (
+                        <div className="mb-6">
+                            <ul className="flex flex-col items-start mb-6 space-y-6 text-lg" data-sb-field-path=".primaryLinks">
+                                {primaryLinks.map((link, index) => (
+                                    <li key={index}>
+                                        <Action {...link} data-sb-field-path={`.${index}`} />
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                    {(socialLinks.length > 0 || props.contacts) && (
+                        <div className="mb-6">
+                            {socialLinks.length > 0 && (
+                                <ul className="flex items-center mb-6 space-x-10" data-sb-field-path=".socialLinks">
+                                    {socialLinks.map((link, index) => (
+                                        <li key={index}>
+                                            <Social {...link} data-sb-field-path={`.${index}`} />
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+                            {props.contacts && <Contacts {...props.contacts} className="mb-6 space-y-4 text-lg" />}
+                        </div>
+                    )}
+                </div>
+            )}
+            <div className="border-t-2 border-current flex flex-col-reverse justify-between pt-6 lg:flex-row">
+            <Markdown
+                options={{ forceInline: true, forceWrapper: true, wrapper: 'p' }}
+                className="sb-markdown"
+                data-sb-field-path=".copyrightText"
+            >
+                {props.copyrightText}
+            </Markdown>
+                {legalLinks.length > 0 && (
+                    <ul className="flex flex-col mb-6 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row" data-sb-field-path=".legalLinks">
+                        {legalLinks.map((link, index) => (
+                            <li key={index}>
+                                <Action {...link} data-sb-field-path={`.${index}`} />
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        </>
+    );
+}
+
+function footerVariantB(props) {
+    const primaryLinks = props.primaryLinks || [];
+    const socialLinks = props.socialLinks || [];
+    const legalLinks = props.legalLinks || [];
+    return (
+        <>
+            <div className="flex flex-col items-center pb-20 text-center">
+                {(props.logo || props.title || props.text) && (
+                    <div className="mb-20">
+                        <Link href="/" className="sb-footer-logo flex items-center justify-center">
+                            {props.logo && (
+                                <ImageBlock {...props.logo} className={classNames('max-h-12', { 'mr-2': props.title })} data-sb-field-path=".logo" />
+                            )}
+                            {props.title && (
+                                <span className="text-3xl font-medium" data-sb-field-path=".title">
+                                    {props.title}
+                                </span>
+                            )}
+                        </Link>
+                        {props.text && (
+                            <Markdown
+                                options={{ forceBlock: true, forceWrapper: true }}
+                                className={classNames('sb-markdown', 'max-w-2xl', { 'mt-8': props.title || props.logo })}
+                                data-sb-field-path=".text"
+                            >
+                                {props.text}
+                            </Markdown>
+                        )}
+                    </div>
+                )}
+                {primaryLinks.length > 0 && (
+                    <div className="w-full max-w-5xl">
+                        <ul className="flex flex-wrap justify-evenly mb-6 text-lg" data-sb-field-path=".primaryLinks">
+                            {primaryLinks.map((link, index) => (
+                                <li key={index} className="mx-4 mb-2">
+                                    <Action {...link} data-sb-field-path={`.${index}`} />
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+                {(socialLinks.length > 0 || props.contacts) && (
+                    <div className="flex flex-wrap justify-evenly">
+                        {socialLinks.length > 0 && (
+                            <ul className="flex items-center justify-center mx-4 mb-4" data-sb-field-path=".socialLinks">
+                                {socialLinks.map((link, index) => (
+                                    <li key={index} className="mx-4 mb-2">
+                                        <Social {...link} data-sb-field-path={`.${index}`} />
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                        {props.contacts && <Contacts {...props.contacts} className="flex flex-wrap justify-center mx-4 mb-4" classNameItem="mx-4 mb-2" />}
+                    </div>
+                )}
+            </div>
+            <div
+                className={classNames(
+                    'border-t-2',
+                    'border-current',
+                    'flex',
+                    'flex-col-reverse',
+                    'items-center',
+                    'pt-8',
+                    'lg:flex-row',
+                    'lg:items-start',
+                    props.copyrightText && legalLinks.length > 0 ? 'lg:justify-between' : 'lg:justify-center'
+                )}
+            >
+                {props.copyrightText && (
+                <Markdown
+                    options={{ forceInline: true, forceWrapper: true, wrapper: 'p' }}
+                    className={classNames('sb-markdown', { 'mt-6 lg:mt-0': legalLinks.length > 0 })}
+                    data-sb-field-path=".copyrightText"
+                >
+                    {props.copyrightText}
+                </Markdown>
+                )}
+                {legalLinks.length > 0 && (
+                    <ul className="flex flex-wrap justify-center" data-sb-field-path=".legalLinks">
+                        {legalLinks.map((link, index) => (
+                            <li key={index} className={classNames('mx-4', 'mb-2', { 'lg:ml-8 lg:mr-0': props.copyrightText })}>
+                                <Action {...link} data-sb-field-path={`.${index}`} />
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        </>
+    );
+}
+
+function Contacts(props) {
+    const cssClasses = props.className || null;
+    const cssClassesItem = props.classNameItem || null;
+    return (
+        <div className={cssClasses} data-sb-field-path=".contacts">
+            {props.phoneNumber && (
+                <p className={cssClassesItem}>
+                    <a
+                        href={`tel:${props.phoneNumber}`}
+                        aria-label={props.phoneAltText}
+                        data-sb-field-path=".phoneNumber .phoneNumber#@href .phoneAltText#@title"
+                    >
+                        {props.phoneNumber}
+                    </a>
+                </p>
+            )}
+            {props.email && (
+                <p className={cssClassesItem}>
+                    <a href={`mailto:${props.email}`} aria-label={props.emailAltText} data-sb-field-path=".email .email#@href .emailAltText#@title">
+                        {props.email}
+                    </a>
+                </p>
+            )}
+            {props.address && (
+                <p className={cssClassesItem}>
+                    <a
+                        href={`https://www.google.com/maps/search/${props.address}`}
+                        aria-label={props.addressAltText}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        data-sb-field-path=".address .address#@href .addressAltText#@title"
+                    >
+                        {props.address}
+                    </a>
+                </p>
+            )}
+        </div>
+    );
 }
 
 function mapMaxWidthStyles(width) {
     switch (width) {
         case 'narrow':
-            return 'max-w-5xl';
-        case 'wide':
             return 'max-w-7xl';
+        case 'wide':
+            return 'max-w-screen-2xl';
         case 'full':
             return 'max-w-full';
     }
